f2aaab82046dee576ee88ce8b96bd1d74473f6bf
Braja <55945388+Brajagopaldas@users.noreply.github.com>
2026-09-07 19:30:18 +0530

    Add StudentResult class to display student info

---
+++ StudentResult.java
@@ -0,0 +1,10 @@
+public class StudentResult {
+   public static void main(String[] args) {
+    
+    char studentGrade = 'A';
+    boolean studentPassed = true;
+
+    System.out.println("Student Grade = " + studentGrade);
+    System.out.println("Student Passed = " + studentPassed);
+   }
+}
