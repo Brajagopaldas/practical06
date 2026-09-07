00bd7ebb729e354b7f67c05302cd2af7d31f8f6f
Brajagopaldas <55945388+Brajagopaldas@users.noreply.github.com>
2026-09-07 12:41:06 +0530

    Add Number class with basic arithmetic operations

---
+++ Number.java
@@ -0,0 +1,20 @@
+public class Number {
+
+    public static void main(String[] args) {
+
+        int a = 100;
+        int b = 50;
+
+        int sum = a + b;
+        int diff = a - b;
+        int mul = a * b;
+        int div = a / b;
+
+        System.out.println("num1 = " + a);
+        System.out.println("num2 = " + b);
+        System.out.println("Addition = " + sum);
+        System.out.println("Difference = " + diff);
+        System.out.println("Multiplication = " + mul);
+        System.out.println("Division = " + div);
+    }
+}
