2b6748281c17bbaac1b893f7b4342a1442b34aa7
Braja <55945388+Brajagopaldas@users.noreply.github.com>
2026-09-07 20:14:08 +0530

    Define PrimitiveDataTypes class

---
+++ PrimitiveDataTypes.java
@@ -0,0 +1,22 @@
+class PrimitiveDataTypes {
+    public static void main(String[] args) {
+
+        byte a = 10;
+        short b = 1000;
+        int c = 50000;
+        long d = 100000L;
+        float e = 25.5f;
+        double f = 95.75;
+        char g = 'A';
+        boolean h = true;
+
+        System.out.println("Byte = " + a);
+        System.out.println("Short = " + b);
+        System.out.println("Int = " + c);
+        System.out.println("Long = " + d);
+        System.out.println("Float = " + e);
+        System.out.println("Double = " + f);
+        System.out.println("Char = " + g);
+        System.out.println("Boolean = " + h);
+    }
+}
