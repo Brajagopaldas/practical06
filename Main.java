ff7de52dbaf7ac35526f8ed2dd63d107995666dc
Brajagopaldas <55945388+Brajagopaldas@users.noreply.github.com>
2026-09-07 11:52:05 +0530

    Add Main class with main method and variable outputs

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main{
+    public static void main(String[] args){
+    
+    int number = 555;
+    float temperature = 45.7f;
+    double salary = 89000.99;
+    char type = 'B';
+
+    System.out.println("Number is :  " + number);
+    System.out.println("Temperature :  " + temperature);
+    System.out.println("Salary :  " + salary);
+    System.out.println("Section :  " + type);
+    }
+}
