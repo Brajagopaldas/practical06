1074b6b2e24294ed403b715c65d20356e8ce9d80
Braja <55945388+Brajagopaldas@users.noreply.github.com>
2026-09-07 19:22:34 +0530

    Add Mark class to calculate and display marks Total,Average

---
+++ Mark.java
@@ -0,0 +1,16 @@
+public class Mark {
+   public static void main(String[] args) {
+    
+    float mark1 = 92.86f;
+    float mark2 = 97.60f;
+    float mark3 = 86.50f;
+    float totalMark = mark1 + mark2 + mark3;
+    float averageMark = totalMark / 3;
+
+    System.out.println("Marks1 =  " + mark1);
+    System.out.println("Marks1 =  " + mark2);
+    System.out.println("Marks1 =  " + mark3);
+    System.out.println("Total Marks =  " + totalMark);
+    System.out.println("Average Marks =  " + averageMark);
+   }
+}
