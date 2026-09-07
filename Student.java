fbb6ccebc3a30c024c216137c7819ffee5db7470
Brajagopaldas <55945388+Brajagopaldas@users.noreply.github.com>
2026-09-07 12:15:59 +0530

    Add Student class with main method

---
+++ Student.java
@@ -0,0 +1,13 @@
+public class Student{
+    public static void main(String[] args){
+        
+        String name = "Ram";
+        int rollNumber = 105;
+        int age = 19;
+
+        System.out.println("Name:  " + name);
+        System.out.println("Rollno:  " + rollNumber);
+        System.out.println("Age:  " + age);
+
+    }
+}
