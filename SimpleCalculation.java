4633a2b277f3a2d7a6a8a5b0e54a6c678d0fbd74
Brajagopaldas <55945388+Brajagopaldas@users.noreply.github.com>
2026-09-07 12:55:38 +0530

    Add SimpleCalculation class for price calculation

---
+++ SimpleCalculation.java
@@ -0,0 +1,13 @@
+public class SimpleCalculation {
+
+    public static void main(String[] args) {
+        
+        double price = 75.99;
+        int quantity = 12;
+        double totalPrice = price * quantity;
+
+        System.out.println("Price =  " + price);
+        System.out.println("Quantity =  " + quantity);
+        System.out.println("Total Price =  " + totalPrice);
+    }
+}
