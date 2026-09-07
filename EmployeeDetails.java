138987847eb24cb40cca1012c6caa789e216b212
Braja <55945388+Brajagopaldas@users.noreply.github.com>
2026-09-07 19:39:55 +0530

    Add EmployeeDetails class with employee information

---
+++ EmployeeDetails.java
@@ -0,0 +1,14 @@
+public class EmployeeDetails {
+   public static void main(String[] args) {
+    
+    String name = "Rajesh";
+    int employeeId = 1246;
+    double salary = 796854.65;
+    char department = 'C';
+
+    System.out.println("Employee Name = " + name);
+    System.out.println("Employee Id = " + employeeId);
+    System.out.println("Salary = " + salary);
+    System.out.println("Department = " + department);
+   }
+}
